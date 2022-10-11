b7fbcd32f7c971263a25de1d87da3864ce83a384
VorotySergey <95651513+SergeyVorotiy@users.noreply.github.com>
2022-10-12 01:37:36 +0300

    Added data from json-server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './styles.css';
 
 document.write('Hello, Webpack!');
+const dataElement = document.querySelector('.data');
+function useRequest (method, url, body = null) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+    xhr.onerror = () => {
+      reject(xhr.response);
+    };
+    xhr.send(JSON.stringify(body));
+  });
+};
+useRequest('GET', 'http://localhost:3000/contacts').then(data => {
+  dataElement.innerHTML = JSON.stringify(data);
+});
